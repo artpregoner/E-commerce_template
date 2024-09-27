e1dc67f3fffe2f7cb9e0c51fa3dd06a7ddf81144
Justine_B. <justine07bagalihog@gmail.com>
2024-09-27 13:10:58 +0800

    Add files via upload

---
+++ signup.blade.php
@@ -0,0 +1,88 @@
+<!doctype html>
+<html lang="en">
+ 
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Concept - Bootstrap 4 Admin Dashboard Template</title>
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="home/assets/vendor/bootstrap/css/bootstrap.min.css">
+    <link href="home/assets/vendor/fonts/circular-std/style.css" rel="stylesheet">
+    <link rel="stylesheet" href="home/assets/libs/css/style.css">
+    <link rel="stylesheet" href="home/assets/vendor/fonts/fontawesome/css/fontawesome-all.css">
+    <style>
+    html,
+    body {
+        height: 100%;
+    }
+
+    body {
+        display: -ms-flexbox;
+        display: flex;
+        -ms-flex-align: center;
+        align-items: center;
+        padding-top: 40px;
+        padding-bottom: 40px;
+    }
+    </style>
+</head>
+<!-- ============================================================== -->
+<!-- signup form  -->
+<!-- ============================================================== -->
+
+<body>
+    <!-- ============================================================== -->
+    <!-- signup form  -->
+    <!-- ============================================================== -->
+    <form class="splash-container">
+        <div class="card">
+            <div class="card-header">
+                <a href="/"><img  src="home/assets/images/logo.png" alt="logo" style="width: 200px; margin-left: auto; margin-right: auto; display: block"></a>
+                <span class="splash-description" style="font-size: 30px"> Registrations Form </span>
+                <span class="splash-description">Please enter your user information.</span>
+            </div>
+            <div class="card-body">
+                <div class="form-group">
+                    <input class="form-control form-control-lg" type="text" name="nick" required="" placeholder="Username" autocomplete="off">
+                </div>
+                <div class="form-group">
+                    <input class="form-control form-control-lg" type="email" name="email" required="" placeholder="E-mail" autocomplete="off">
+                </div>
+                <div class="form-group">
+                    <input class="form-control form-control-lg" id="pass1" type="password" required="" placeholder="Password">
+                </div>
+                <div class="form-group">
+                    <input class="form-control form-control-lg" required="" placeholder="Confirm">
+                </div>
+                <div class="form-group pt-2">
+                    <button class="btn btn-block btn-primary" type="submit" style="background-color: #463426">Register My Account</button>
+                </div>
+                <div class="form-group">
+                    <label class="custom-control custom-checkbox">
+                        <input class="custom-control-input" type="checkbox"><span class="custom-control-label">By creating an account, you agree the <a href="#">terms and conditions</a></span>
+                    </label>
+                <div class="card-body p-0" style="margin-left: 40px">
+                    <ul class="social-sales list-group list-group-flush">
+                        <li class="list-group-item social-sales-content"><span
+                                class="social-sales-icon-circle facebook-bgcolor mr-4" ><i
+                                    class="fab fa-facebook-f"></i></span>
+                                    <span
+                                class="social-sales-icon-circle twitter-bgcolor mr-4" ><i
+                                    class="fab fa-twitter"></i></span>
+                                    <span
+                                class="social-sales-icon-circle instagram-bgcolor mr-4"><i
+                                    class="fab fa-instagram"></i></span>
+                        </li>
+                    </ul>
+                </div>
+                </div>
+            <div class="card-footer bg-white">
+                <p>Already member? <a href="/logins" class="text-secondary">Login Here.</a></p>
+            </div>
+        </div>
+    </form>
+</body>
+
+ 
+</html>
