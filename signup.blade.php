5c4ad97461b92dd36c86f132b89af2c859b95d96
Justine_B. <justine07bagalihog@gmail.com>
2024-09-28 08:00:23 +0800

    Update signup.blade.php

--- signup.blade.php
+++ signup.blade.php
@@ -35,25 +35,47 @@
     <!-- ============================================================== -->
     <!-- signup form  -->
     <!-- ============================================================== -->
-    <form class="splash-container">
-        <div class="card">
+    <form class="splash-container" >
+        <div class="card" style=" width: 150%" >
             <div class="card-header">
                 <a href="/"><img  src="home/assets/images/logo.png" alt="logo" style="width: 200px; margin-left: auto; margin-right: auto; display: block"></a>
-                <span class="splash-description" style="font-size: 30px"> Registrations Form </span>
+                <span class="splash-description" style="font-size: 30px;"> Registrations Form </span>
                 <span class="splash-description">Please enter your user information.</span>
             </div>
-            <div class="card-body">
-                <div class="form-group">
-                    <input class="form-control form-control-lg" type="text" name="nick" required="" placeholder="Username" autocomplete="off">
-                </div>
-                <div class="form-group">
-                    <input class="form-control form-control-lg" type="email" name="email" required="" placeholder="E-mail" autocomplete="off">
-                </div>
-                <div class="form-group">
-                    <input class="form-control form-control-lg" id="pass1" type="password" required="" placeholder="Password">
-                </div>
-                <div class="form-group">
-                    <input class="form-control form-control-lg" required="" placeholder="Confirm">
+            <div class="card-body" >
+                <div class="row">
+                    <div class="col">
+                        <div class="form-group">
+                            <input class="form-control form-control-lg" type="text" name="name" placeholder="Name" required autofocus autocomplete="name">
+                        </div>
+                        <div class="form-group">
+                            <input class="form-control form-control-lg" name="phone" required="" placeholder="Phone" required autocomplete="tel">
+                        </div>
+                        <div class="form-group">
+                            <input class="form-control form-control-lg" type="email" name="email" placeholder="E-mail" required autocomplete="email">
+                        </div>
+                        <div class="form-group">
+                            <input class="form-control form-control-lg" list="gender" name="gender" placeholder="Gender" required autocomplete="off">
+                            <datalist id="gender">
+                                <option value="Female"></option>
+                                <option value="Male"></option>
+                              </datalist>
+                         </div>
+                    </div>
+                    <div class="col">
+                        <div class="form-group">
+                            <input class="form-control form-control-lg" type="text" name="bday" placeholder="Birthdate" onfocus="this.type='date'" onblur="this.type='text'" autocomplete="off">
+                        </div>
+                        <div class="form-group">
+                            <input class="form-control form-control-lg" type="text" name="address" placeholder="Address" required autocomplete="street-address">
+                        </div>
+                        <div class="form-group">
+                            <input class="form-control form-control-lg" id="pass1" type="password" placeholder="Password" required>
+                        </div>
+                        <div class="form-group">
+                            <input class="form-control form-control-lg" type="password" required placeholder="Confirm">
+                        </div>
+                    </div>
                 </div>
                 <div class="form-group pt-2">
                     <button class="btn btn-block btn-primary" type="submit" style="background-color: #463426">Register My Account</button>
@@ -62,23 +84,16 @@
                     <label class="custom-control custom-checkbox">
                         <input class="custom-control-input" type="checkbox"><span class="custom-control-label">By creating an account, you agree the <a href="#">terms and conditions</a></span>
                     </label>
-                <div class="card-body p-0" style="margin-left: 40px">
-                    <ul class="social-sales list-group list-group-flush">
-                        <li class="list-group-item social-sales-content"><span
-                                class="social-sales-icon-circle facebook-bgcolor mr-4" ><i
-                                    class="fab fa-facebook-f"></i></span>
-                                    <span
-                                class="social-sales-icon-circle twitter-bgcolor mr-4" ><i
-                                    class="fab fa-twitter"></i></span>
-                                    <span
-                                class="social-sales-icon-circle instagram-bgcolor mr-4"><i
-                                    class="fab fa-instagram"></i></span>
-                        </li>
-                    </ul>
-                </div>
-                </div>
+            
             <div class="card-footer bg-white">
+                <div class="row" >
                 <p>Already member? <a href="/logins" class="text-secondary">Login Here.</a></p>
+                    <div id="icons">
+                        <a href="#" class="item" ><i class="fab fa-facebook-f mr-4" style="font-size: 150%" ></i></a>
+                        <a href="#" class="item" ><i class="fab fa-twitter mr-4" style="font-size: 150%" ></i></a>
+                        <a href="#" class="item" ><i class="fab fa-instagram " style="font-size: 150%" ></i></a>
+                    </div>
+                </div>
             </div>
         </div>
     </form>
